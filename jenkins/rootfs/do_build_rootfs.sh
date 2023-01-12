d50d25a8300711393878cf6b591adb663d6f9b65
lichaoran <pkwarcraft@gmail.com>
2023-01-12 10:43:17 +0800

    using black and isort to make code cleaner
    Signed-off-by: lichaoran <pkwarcraft@gmail.com>

--- jenkins/rootfs/do_build_rootfs.sh
+++ jenkins/rootfs/do_build_rootfs.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+set -eE
+trap cleanup ERR
+
+function cleanup() {
+    [ ! -z "$(docker buildx ls |grep buildx-$release-$arch)" ] && docker buildx rm buildx-$release-$arch
+}
+
 [ -d $WORKSPACE/outdir/ ] && rm -rvf $WORKSPACE/outdir/* || mkdir -pv $WORKSPACE/outdir
+docker info
 docker buildx ls
 docker ps -a
-[ -z "$(docker buildx ls |grep buildx-$release-$arch)" ] && docker buildx create --bootstrap --use --name buildx-$release-$arch
-
+[ -z "$(docker buildx ls |grep buildx-$release-$arch)" ] && docker buildx create --bootstrap --use --name buildx-$release-$arch || docker buildx use buildx-$release-$arch
 # build rootfs docker image
 docker buildx build --build-arg REL_TAG=$release --platform linux/$arch --tag openeuler-wsl:$release --squash --cache-from=type=local,src=/var/cache/buildx/$release-$arch --cache-to=type=local,dest=/var/cache/buildx/$release-$arch \
     -o type=tar,dest=$WORKSPACE/outdir/$release-$arch.tar $WORKSPACE/docker/
 docker run --rm -v $WORKSPACE/:/wd bytesco/pigz -9 -v -Y -f /wd/outdir/$release-$arch.tar
-
-[ ! -z "$(docker buildx ls |grep buildx-$release-$arch)" ] && docker buildx rm buildx-$release-$arch
--- submit.py
+++ submit.py
@@ -1,45 +1,67 @@
 # coding=utf-8
-import os
+"""
+This script try to submit the zipped WSL app to Windows Store
+Using the [API](https://learn.microsoft.com/en-us/windows/uwp/monetize/manage-app-submissions)
+"""
+import argparse
+import datetime
 import json
+import logging
+import os
+import sys
 import time
+from string import Template
+from typing import Optional
+
+import humanize
 import requests
 from azure.storage.blob import BlobClient
-from string import Template
-import argparse
-import sys
-import datetime
-from typing import Optional
-import humanize
-import datetime
-import logging
 
 # setup logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
-fmter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+fmter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
 ch.setFormatter(fmt=fmter)
 logger.addHandler(ch)
 
-class SubmitPackage(object):
+REQUEST_TIMEOUT = 60
+
+class SubmitPackage:
+    """
+    submit the wsl app
+    """
+
     def __init__(self, tenantId: str, clientId: str, clientSecret: str) -> None:
         self.baseurl = tokenResource = "https://manage.devcenter.microsoft.com"
-        tokenRequestBody = "grant_type=client_credentials&client_id={}&client_secret={}&resource={}".format(clientId, clientSecret, tokenResource)
-        tokenHeaders = {"Content-Type": "application/x-www-form-urlencoded; charset=utf-8"}
-        tokenResponse = requests.post("https://login.microsoftonline.com/{}/oauth2/token".format(tenantId), data=tokenRequestBody, headers=tokenHeaders)
-        
-        logger.info('get access token status: {} {}'.format(tokenResponse.status_code, tokenResponse.reason))
+        tokenRequestBody = f"grant_type=client_credentials&client_id={clientId}&" \
+                            f"client_secret={clientSecret}&resource={tokenResource}"
+        tokenHeaders = {
+            "Content-Type": "application/x-www-form-urlencoded; charset=utf-8"
+        }
+        tokenResponse = requests.post(
+            f"https://login.microsoftonline.com/{tenantId}/oauth2/token",
+            data=tokenRequestBody,
+            headers=tokenHeaders,
+            timeout=REQUEST_TIMEOUT
+        )
+
+        logger.info(
+            "get access token status: %s %s",
+            tokenResponse.status_code,
+            tokenResponse.reason,
+        )
         tokenJson = tokenResponse.json()
         self._acess_token = tokenJson["access_token"]
         self._init = True
 
     def make_submit_body(self, metapath: str, templatepath: str):
-        with open(metapath, encoding='utf-8') as meta:
+        with open(metapath, encoding="utf-8") as meta:
             metadata = json.loads(meta.read())
-            metadata['year'] = datetime.date.today().year
-        with open(templatepath, encoding='utf-8') as template:
-            content = ''.join(template.readlines())
+            metadata["year"] = datetime.date.today().year
+        with open(templatepath, encoding="utf-8") as template:
+            content = "".join(template.readlines())
             t = Template(content)
             config = t.substitute(metadata)
         return json.loads(config)
@@ -47,61 +69,114 @@ class SubmitPackage(object):
     def get_app_info(self, release: str):
         if not self._init:
             sys.exit(1)
-        headers = {"Authorization": "Bearer " + self._acess_token,
-                "Content-type": "application/json",
-                "User-Agent": "Python"}
+        headers = {
+            "Authorization": "Bearer " + self._acess_token,
+            "Content-type": "application/json",
+            "User-Agent": "Python",
+        }
 
-        appResponse = requests.get("{}/v1.0/my/applications".format(self.baseurl), headers=headers)
+        appResponse = requests.get(
+            f"{self.baseurl}/v1.0/my/applications", headers=headers, timeout=REQUEST_TIMEOUT
+        )
 
-        logger.info('get app info status: {} {}'.format(appResponse.status_code, appResponse.reason))
-        logger.info(appResponse.headers["MS-CorrelationId"])  # Log correlation ID
+        logger.info(
+            "get app info status: %s %s", appResponse.status_code, appResponse.reason
+        )
+        # Log correlation ID
+        logger.info(appResponse.headers["MS-CorrelationId"])
         data = appResponse.json()
-        for app in data['value']:
-            if app['primaryName'] == 'openEuler {}'.format(release):
+        for app in data["value"]:
+            if app["primaryName"] == f"openEuler {release}":
                 return app
-        raise ValueError("no {} found".format(release))
+        raise ValueError(f"no {release} found")
 
     def delete_exist_submission(self, submissionToRemove: str):
         if not self._init:
             sys.exit(1)
-        headers = {"Authorization": "Bearer " + self._acess_token,
-                "Content-type": "application/json",
-                "User-Agent": "Python"}
+        headers = {
+            "Authorization": "Bearer " + self._acess_token,
+            "Content-type": "application/json",
+            "User-Agent": "Python",
+        }
 
-        deleteSubmissionResponse = requests.delete("{}/v1.0/my/{}".format(self.baseurl, submissionToRemove), headers=headers)
+        deleteSubmissionResponse = requests.delete(
+            f"{self.baseurl}/v1.0/my/{submissionToRemove}", headers=headers,
+            timeout=REQUEST_TIMEOUT
+        )
 
-        logger.info('delete pending submit status: {} {}'.format(deleteSubmissionResponse.status_code, deleteSubmissionResponse.reason))
-        logger.info(deleteSubmissionResponse.headers["MS-CorrelationId"])  # Log correlation ID
+        logger.info(
+            "delete pending submit status: %s %s",
+            deleteSubmissionResponse.status_code,
+            deleteSubmissionResponse.reason,
+        )
+        # Log correlation ID
+        logger.info(deleteSubmissionResponse.headers["MS-CorrelationId"])
 
-    def create_submit(self, applicationId: str, appSubmissionRequestJson, zipFilePath: str):
-        headers = {"Authorization": "Bearer " + self._acess_token,
-                "Content-type": "application/json",
-                "User-Agent": "Python"}
+    def create_submit(
+        self, applicationId: str, appSubmissionRequestJson, zipFilePath: str
+    ):
+        """
+        Do the submit process:
+        1. create a submission
+        2. fill the submission metadata
+        3. upload blob file
+        4. commit the submission
+        5. wait the submission accepted/denied by server
+        """
+        headers = {
+            "Authorization": "Bearer " + self._acess_token,
+            "Content-type": "application/json",
+            "User-Agent": "Python",
+        }
 
         # Create submission
-        createSubmissionResponse = requests.post("{}/v1.0/my/applications/{}/submissions".format(self.baseurl, applicationId), headers=headers)
+        createSubmissionResponse = requests.post(
+            f"{self.baseurl}/v1.0/my/applications/{applicationId}/submissions",
+            headers=headers, timeout=REQUEST_TIMEOUT
+        )
 
-        logger.info('create submission status: {} {}'.format(createSubmissionResponse.status_code, createSubmissionResponse.reason))
-        logger.info(createSubmissionResponse.headers["MS-CorrelationId"])  # Log correlation ID
+        logger.info(
+            "create submission status: %s %s",
+            createSubmissionResponse.status_code,
+            createSubmissionResponse.reason,
+        )
+        # Log correlation ID
+        logger.info(createSubmissionResponse.headers["MS-CorrelationId"])
 
         submissionJsonObject = createSubmissionResponse.json()
         if createSubmissionResponse.status_code >= 400:
             logger.error(submissionJsonObject)
-            sys.exit(1) 
+            sys.exit(1)
         submissionId = submissionJsonObject["id"]
         fileUploadUrl = submissionJsonObject["fileUploadUrl"]
 
-        if len(submissionJsonObject['applicationPackages']) > 0:
-            for package in submissionJsonObject['applicationPackages']:
-                logger.info('package {} {} {} will be replaced'.format(package['fileName'], package['fileStatus'], package['version']))
-                package['fileStatus'] = 'PendingDelete'
+        if len(submissionJsonObject["applicationPackages"]) > 0:
+            for package in submissionJsonObject["applicationPackages"]:
+                logger.info(
+                    "package %s %s %s will be replaced",
+                    package["fileName"],
+                    package["fileStatus"],
+                    package["version"],
+                )
+                package["fileStatus"] = "PendingDelete"
 
-        appSubmissionRequestJson['applicationPackages'].extend(submissionJsonObject['applicationPackages'])
+        appSubmissionRequestJson["applicationPackages"].extend(
+            submissionJsonObject["applicationPackages"]
+        )
 
         # Update submission
-        updateSubmissionResponse = requests.put("{}/v1.0/my/applications/{}/submissions/{}".format(self.baseurl, applicationId, submissionId), json.dumps(appSubmissionRequestJson).encode('utf-8'), headers=headers)
-        logger.info('update submission status: {} {}'.format(updateSubmissionResponse.status_code, updateSubmissionResponse.reason))
-        logger.info(updateSubmissionResponse.headers["MS-CorrelationId"])  # Log correlation ID
+        updateSubmissionResponse = requests.put(
+            f"{self.baseurl}/v1.0/my/applications/{applicationId}/submissions/{submissionId}",
+            json.dumps(appSubmissionRequestJson).encode("utf-8"),
+            headers=headers, timeout=REQUEST_TIMEOUT
+        )
+        logger.info(
+            "update submission status: %s %s",
+            updateSubmissionResponse.status_code,
+            updateSubmissionResponse.reason,
+        )
+        # Log correlation ID
+        logger.info(updateSubmissionResponse.headers["MS-CorrelationId"])
 
         updateSubmissionObject = updateSubmissionResponse.json()
         if updateSubmissionResponse.status_code >= 400:
@@ -109,78 +184,127 @@ class SubmitPackage(object):
             sys.exit(1)
 
         size = os.stat(zipFilePath).st_size
-        logger.info("begin to upload zip file({}): {} to {}".format(humanize.naturalsize(size), zipFilePath, fileUploadUrl))
+        logger.info(
+            "begin to upload zip file(%s): %s to %s",
+            humanize.naturalsize(size),
+            zipFilePath,
+            fileUploadUrl,
+        )
         # Upload images and packages in a zip file.
         blob_client = BlobClient.from_blob_url(fileUploadUrl)
         with open(zipFilePath, "rb") as data:
-            blob_client.upload_blob(data, blob_type="BlockBlob", progress_hook=progress, length=size)
+            blob_client.upload_blob(
+                data, blob_type="BlockBlob", progress_hook=progress, length=size
+            )
 
         logger.info("upload end")
         # Commit submission
-        commitResponse = requests.post("{}/v1.0/my/applications/{}/submissions/{}/commit".format(self.baseurl, applicationId, submissionId), headers=headers)
+        commitResponse = requests.post(
+            f"{self.baseurl}/v1.0/my/applications/{applicationId}/" \
+                "submissions/{submissionId}/commit",
+            headers=headers, timeout=REQUEST_TIMEOUT
+        )
 
-        logger.info('commit submission status: {} {}'.format(commitResponse.status_code, commitResponse.reason))
-        logger.info(commitResponse.headers["MS-CorrelationId"])  # Log correlation ID
+        logger.info(
+            "commit submission status: %s %s",
+            commitResponse.status_code,
+            commitResponse.reason,
+        )
+        # Log correlation ID
+        logger.info(commitResponse.headers["MS-CorrelationId"])
         commitResponseObject = commitResponse.json()
         if commitResponse.status_code >= 400:
             logger.error(commitResponseObject)
             sys.exit(1)
 
         # Pull submission status until commit process is completed
-        getSubmissionStatusResponse = requests.get("{}/v1.0/my/applications/{}/submissions/{}/status".format(self.baseurl, applicationId, submissionId), headers=headers)
+        getSubmissionStatusResponse = requests.get(
+            f"{self.baseurl}/v1.0/my/applications/{applicationId}" \
+                "/submissions/{submissionId}/status",
+            headers=headers, timeout=REQUEST_TIMEOUT
+        )
         submissionJsonObject = getSubmissionStatusResponse.json()
         while submissionJsonObject["status"] == "CommitStarted":
             time.sleep(60)
-            getSubmissionStatusResponse = requests.get("{}/v1.0/my/applications/{}/submissions/{}/status".format(self.baseurl, applicationId, submissionId), headers=headers)
+            getSubmissionStatusResponse = requests.get(
+                f"{self.baseurl}/v1.0/my/applications/{applicationId}/" \
+                    "submissions/{submissionId}/status",
+                headers=headers, timeout=REQUEST_TIMEOUT
+            )
             submissionJsonObject = getSubmissionStatusResponse.json()
-            logger.info('get commit status: {}'.format(submissionJsonObject["status"]))
+            logger.info("get commit status: %s", submissionJsonObject["status"])
 
         exitCode = 0
-        logger.info('get commit status finished, final status: {}'.format(submissionJsonObject["status"]))
-        if submissionJsonObject['statusDetails']["errors"]:
+        logger.info(
+            "get commit status finished, final status: %s",
+            submissionJsonObject["status"],
+        )
+        if submissionJsonObject["statusDetails"]["errors"]:
             logger.error(submissionJsonObject)
             exitCode = 1
 
         return exitCode
 
+
 def progress(current: int, total: Optional[int]):
-    t = total if total else '0'
-    logger.info('{}/{}'.format(humanize.naturalsize(current), humanize.naturalsize(t)))
+    t = total if total else "0"
+    logger.info("%s/%s", humanize.naturalsize(current), humanize.naturalsize(t))
+
 
 def init_parser():
-    parser = argparse.ArgumentParser(
-        prog = 'submit.py',
-        description= 'automate create a UWP app submission',
+    new_parser = argparse.ArgumentParser(
+        prog="submit.py",
+        description="automate create a UWP app submission",
     )
 
-    parser.add_argument('-c', '--client_id', help="azure AD applicaion client id")
-    parser.add_argument('-t', '--tenant_id', help="azure AD user id")
-    parser.add_argument('-k', '--client_secret', help="azure AD application key secret")
-    parser.add_argument('-r', '--release', help="release number")
-    parser.add_argument('-m', '--meta', help="meta data file for submission request template, default to ./meta/{RELEASE}/meta.json")
-    parser.add_argument('--template', default="template.json", help="submission request template, default to ./template.json")
-    parser.add_argument('-f', '--zipfile', help="zip file path which contains intro images name as `release` and a appxupload bundle")
-    return parser
+    new_parser.add_argument("-c", "--client_id", help="azure AD applicaion client id")
+    new_parser.add_argument("-t", "--tenant_id", help="azure AD user id")
+    new_parser.add_argument(
+        "-k", "--client_secret", help="azure AD application key secret"
+    )
+    new_parser.add_argument("-r", "--release", help="release number")
+    new_parser.add_argument(
+        "-m",
+        "--meta",
+        help="submission request template file, default to ./meta/{RELEASE}/meta.json",
+    )
+    new_parser.add_argument(
+        "--template",
+        default="template.json",
+        help="submission request template, default to ./template.json",
+    )
+    new_parser.add_argument(
+        "-f",
+        "--zipfile",
+        help="zip file path which contains intro images name as `release` and a appxupload bundle",
+    )
+    return new_parser
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = init_parser()
     args = parser.parse_args()
 
-    if not args.client_id or not args.tenant_id or not args.client_secret or not args.release or not args.zipfile:
+    if (
+        not args.client_id
+        or not args.tenant_id
+        or not args.client_secret
+        or not args.release
+        or not args.zipfile
+    ):
         parser.print_help()
         sys.exit(1)
 
     if not args.meta:
-        args.meta = 'meta/{}/meta.json'.format(args.release)
-    
-    sp = SubmitPackage(args.tenant_id, args.client_id, args.client_secret)
-    
-    data = sp.get_app_info(args.release)
+        args.meta = f"meta/{args.release}/meta.json"
 
-    if data and "pendingApplicationSubmission" in data :
-        submissionToRemove = data["pendingApplicationSubmission"]["resourceLocation"]
+    sp = SubmitPackage(args.tenant_id, args.client_id, args.client_secret)
+
+    release_data = sp.get_app_info(args.release)
+
+    if release_data and "pendingApplicationSubmission" in release_data:
+        submissionToRemove = release_data["pendingApplicationSubmission"]["resourceLocation"]
         sp.delete_exist_submission(submissionToRemove)
 
     requestBody = sp.make_submit_body(args.meta, args.template)
-    sys.exit(sp.create_submit(data['id'], requestBody, args.zipfile))
-    
+    sys.exit(sp.create_submit(release_data["id"], requestBody, args.zipfile))
