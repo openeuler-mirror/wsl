f3a59aa84af79cf7e35d3f689b034aedac74ebe9
okrc <okrc@hexo.dev>
2023-03-15 20:30:11 +0800

    rootfs 瘦身

--- generate_rootfs.sh
+++ generate_rootfs.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 ARGS=$1
-if [ -z $ARGS ];then
+if [ -z $ARGS ]; then
     ARGS="latest"
 fi
 
-docker run openeuler-wsl:$ARGS echo hello
 echo exporting... this may take 2 minuts, please wait...
-docker export $(docker ps -ql) | gzip -9 > ./install.tar.gz
+docker run --rm openeuler-wsl:$ARGS >./install.tar.gz
