d281eb5a6a5012ae996a193552f8cf7eff60e96b
baixu <baixu_yewu@cmss.chinamobile.com>
2024-10-08 15:30:44 +0800

    fixed some spelling errors

--- generate_rootfs.sh
+++ generate_rootfs.sh
@@ -5,5 +5,5 @@ if [ -z $ARGS ]; then
     ARGS="latest"
 fi
 
-echo exporting... this may take 2 minuts, please wait...
+echo exporting... this may take 2 minutes, please wait...
 docker run --rm openeuler-wsl:$ARGS >./install.tar.gz
